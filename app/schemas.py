1b9bb6b5178e0f06177212fca65ae1ce98b80f0e
Maharshi <maharshilavana@gmail.com>
2026-01-27 00:22:39 +0530

    Fices the error of wrong import

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel , EmailStr
+from typing import Optional,List
 
 class User_Create(BaseModel):
     username : str
@@ -8,3 +9,13 @@ class User_Create(BaseModel):
 class User_Login(BaseModel):
     identifier : str | EmailStr
     password : str
+
+class Tweet_create(BaseModel):
+    content : str
+
+class User_Profile(BaseModel):
+    username : str
+    bio : Optional[str] = None
+    followers : int = 0
+    following : int =  0
+    tweets : List[Tweet_create] = []
